e7ae09d4460fd3e4aae72f3d9ccdd98001176810
deltah404 <oloskossi@gmail.com>
2022-06-10 09:33:45 +0100

    Further example.py edits for clarity

--- example.py
+++ example.py
@@ -1,5 +1,18 @@
 from arrows_n_dots import Interpreter
 
 i = Interpreter()
-script = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" # causes a PointerIndexError - attempts to move pointer position outside of tape range
-i.evaluate(script)
+
+#######################
+
+ex_script_1 = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" #       X causes a PointerIndexError - attempts to move pointer position outside of tape range
+i.evaluate(ex_script_1)
+
+#######################
+
+ex_script_2 = "<" #                                                          X causes a PointerIndexError - attempts to move pointer position outside of tape range
+i.evaluate(ex_script_2)
+
+#######################
+
+ex_script_3 = "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^." #                    - valid AND code, outputs "&"
+i.evaluate(ex_script_3)
